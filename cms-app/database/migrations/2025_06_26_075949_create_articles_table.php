ecc2d1b1c5e29820dcbeeb5771fb2252eb8150a8
DeanAnhDev <dinhtheanhts29012005@gmail.com>
2025-06-26 15:10:45 +0700

    create models

---
+++ cms-app/database/migrations/2025_06_26_075949_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 500);
+            $table->string('slug', 255)->unique();
+            $table->longText('content'); // HTML nội dung
+            $table->string('thumbnail', 255)->nullable(); // ảnh đại diện
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->unsignedBigInteger('author_id');
+            $table->enum('status', ['draft', 'published', 'archived'])->default('draft');
+            $table->integer('views')->default(0);
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->nullOnDelete();
+            $table->foreign('author_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};

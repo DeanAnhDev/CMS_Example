0266db400792eace87da2df23a9c122d1f338e5e
DeanAnhDev <dinhtheanhts29012005@gmail.com>
2025-06-30 11:45:23 +0700

    authen and author user

--- cms-app/routes/web.php
+++ cms-app/routes/web.php
@@ -4,11 +4,14 @@ use App\Http\Controllers\ArticlesController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\CkeditorController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\CheckRole;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -22,22 +25,50 @@ Route::middleware('auth')->group(function () {
 
 
 //categories
-Route::get('categories/{category:slug}/edit', [CategoriesController::class, 'edit'])->name('categories.edit');
-Route::put('categories/{category:slug}', [CategoriesController::class, 'update'])->name('categories.update');
-Route::delete('categories/{category:slug}', [CategoriesController::class, 'destroy'])->name('categories.destroy');
-Route::get('categories', [CategoriesController::class, 'index'])->name('categories.index');
-Route::get('categories/create', [CategoriesController::class, 'create'])->name('categories.create');
-Route::post('categories', [CategoriesController::class, 'store'])->name('categories.store');
+Route::middleware([
+    'auth',
+    'verified',
+    CheckRole::class . ':admin'
+])->group(function () {
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+    Route::get('categories/create', [CategoriesController::class, 'create'])->name('categories.create');
+    Route::post('categories', [CategoriesController::class, 'store'])->name('categories.store');
+    Route::get('categories/{category:slug}/edit', [CategoriesController::class, 'edit'])->name('categories.edit');
+    Route::put('categories/{category:slug}', [CategoriesController::class, 'update'])->name('categories.update');
+    Route::delete('categories/{category:slug}', [CategoriesController::class, 'destroy'])->name('categories.destroy');
+    Route::get('categories', [CategoriesController::class, 'index'])->name('categories.index');
+});
+
+
+
 
 //articles
-Route::get('articles/{article:slug}/edit', [ArticlesController::class, 'edit'])->name('articles.edit');
-Route::put('articles/{article:slug}', [ArticlesController::class, 'update'])->name('articles.update');
-Route::delete('articles/{article:slug}', [ArticlesController::class, 'destroy'])->name('articles.destroy');
-Route::get('articles', [ArticlesController::class, 'index'])->name('articles.index');
-Route::get('articles/create', [ArticlesController::class, 'create'])->name('articles.create');
-Route::post('articles', [ArticlesController::class, 'store'])->name('articles.store');
+
+Route::middleware([
+    'auth',
+    'verified',
+    CheckRole::class . ':admin'
+])->group(function () {
+    Route::get('/admin', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+    Route::get('articles/{article:slug}/edit', [ArticlesController::class, 'edit'])->name('articles.edit');
+    Route::put('articles/{article:slug}', [ArticlesController::class, 'update'])->name('articles.update');
+    Route::delete('articles/{article:slug}', [ArticlesController::class, 'destroy'])->name('articles.destroy');
+    Route::get('articles', [ArticlesController::class, 'index'])->name('articles.index');
+    Route::get('articles/create', [ArticlesController::class, 'create'])->name('articles.create');
+    Route::post('articles', [ArticlesController::class, 'store'])->name('articles.store');
+});
 
 //upload
-Route::post('/ckeditor/upload', [CkeditorController::class, 'upload'])->name('ckeditor.upload');
+Route::post('/ckeditor/upload', [CkeditorController::class, 'upload'])
+    ->middleware([
+        'auth',
+        'verified',
+        CheckRole::class . ':admin'
+    ])
+    ->name('ckeditor.upload');
 
 require __DIR__.'/auth.php';
